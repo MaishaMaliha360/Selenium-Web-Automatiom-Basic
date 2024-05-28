580952b6e4b583df423f7003d83f21b895406f95
unknown <maisha.annika360@gmail.com>
2024-05-28 19:17:09 +0600

    1st day

---
+++ src/test/java/com/maisha/maliha/Web_Automation_Basic/BaseDriver.java
@@ -0,0 +1,43 @@
+package com.maisha.maliha.Web_Automation_Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseDriver {
+	
+	WebDriver driver;
+	
+	@BeforeSuite
+
+	public void startBrowser() {
+		
+		String browser_name = System.getProperty("browser","chrome");
+		
+		if(browser_name.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}else if(browser_name.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+	}else {
+		WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+	
+          }
+        }
+	
+	@AfterSuite
+	public void closeBrowser() {
+		driver.close();
+	}
+	
+	}
